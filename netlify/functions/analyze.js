e1350437ce4d684262f26cea56370af873f1d405
luckyfay5 <111223397+luckyfay5@users.noreply.github.com>
2026-04-22 11:06:21 +0800

    Update analyze.js

--- netlify/functions/analyze.js
+++ netlify/functions/analyze.js
@@ -13,7 +13,8 @@ exports.handler = async function(event, context) {
       };
     }
 
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${apiKey}`, {
+    // 将这里的模型名称改为了对公众 API Key 开放的 gemini-1.5-flash
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: event.body 
